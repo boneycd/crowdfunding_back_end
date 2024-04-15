8a4fccbba1112ab4bcb835be6438d424b383a8b5
boneycd <147505486+boneycd@users.noreply.github.com>
2024-04-15 19:13:28 +0800

    TestCommit

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -27,12 +27,7 @@ class ProjectList(APIView):
             status=status.HTTP_400_BAD_REQUEST
         )
 
-    def delete(self, request,pk):
-        project = self.get_object(pk)
-        project.delete()
-        return Response(
-            status = status.HTTP_204_NO_CONTENT
-        )
+
 
 
 
